281bb7a148decfc66d9bf0ef4dfb1d9850e54353
nemo D'acremont <nemo@nemo.dacremont@gmail.com>
2020-11-24 07:34:21 +0000

    Imported Fonts, added colors scss variables

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <div id="nav">
-    <router-link to="/">All</router-link> |
-    <router-link to="/active">Active</router-link> |
-    <router-link to="/completed">Completed</router-link>
-  </div>
-  <router-view/>
+	<header>
+		<h1>#todo</h1>
+		<nav id="nav">
+			<router-link to="/">All</router-link> |
+			<router-link to="/active">Active</router-link> |
+			<router-link to="/completed">Completed</router-link>
+		</nav>
+	</header>
+
+	<main>
+		<router-view/>
+	</main>
+
+	<footer>
+		Nemo D'acremont | <a target="_blank" href="https://github.com/odasta/">https://github.com/odasta/</a>
+	</footer>
 </template>
 
 <style lang="scss">
+
+// Import Fonts
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Raleway&display=swap');
+
+// Colors
+
+$red: #EB5757;
+$blue: #2F80ED;
+$dark-gray: #333333;
+$gray-3: #828282;
+$gray-4: #BDBDBD;
+$gray-5: #A9A9A9;
+$background-color: #FFF;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,16 +41,16 @@
   color: #2c3e50;
 }
 
-#nav {
+nav {
   padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+	a {
+		font-weight: bold;
+		color: #2c3e50;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
